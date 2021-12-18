2c86c40a3aa1371aea99f2d4ffbcf03a16e65972
Evelyn Carolina <evelyncarolina.contato@gmail.com>
2021-12-18 20:38:11 -0300

    [Feature - Task: Adicionado Projeto Base]

---
+++ project/src/main/java/com/plany/project/ProjectplanyApplication.java
@@ -0,0 +1,13 @@
+package com.plany.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectplanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectplanyApplication.class, args);
+	}
+
+}
