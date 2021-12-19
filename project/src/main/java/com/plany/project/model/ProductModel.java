64f834ea00058d5e149ee43b7f31424f8e2a9556
Evelyn Carolina <evelyncarolina.contato@gmail.com>
2021-12-18 21:33:04 -0300

    [Feature - Task: Created Get & Set Product]

--- project/src/main/java/com/plany/project/model/ProductModel.java
+++ project/src/main/java/com/plany/project/model/ProductModel.java
@@ -15,7 +15,29 @@ public class ProductModel {
 	private @NotBlank String description;
 	private @NotNull Double price;
 	
+	public Long getIdProduct() {
+		return idProduct;
+	}
+	public void setIdProduct(Long idProduct) {
+		this.idProduct = idProduct;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
 	
-	
-
 }
