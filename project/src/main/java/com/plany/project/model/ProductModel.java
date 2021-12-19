65d80399e7fea083218152144ce4bdb6f154d6a6
Evelyn Carolina <evelyncarolina.contato@gmail.com>
2021-12-18 21:28:05 -0300

    [Feature - Task: Created Variable]

--- project/src/main/java/com/plany/project/model/ProductModel.java
+++ project/src/main/java/com/plany/project/model/ProductModel.java
@@ -1,5 +1,21 @@
 package com.plany.project.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class ProductModel {
+	
+	private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long idProduct;
+	private @NotBlank String name;
+	private @NotBlank String description;
+	private @NotNull Double price;
+	
+	
+	
 
 }
