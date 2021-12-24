72edc38caa8126378f62a017a198401ed1221a9b
Evelyn Carolina <evelyncarolina.contato@gmail.com>
2021-12-24 00:03:01 -0300

    [Feature - Task: Criado Método Post de Produto]

---
+++ project/src/main/java/com/plany/project/control/ProductControl.java
@@ -0,0 +1,50 @@
+package com.plany.project.control;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.plany.project.model.ProductModel;
+import com.plany.project.repository.ProductRepository;
+import com.plany.project.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductControl {
+	
+	private @Autowired ProductRepository repository;
+	private @Autowired ProductService service;
+	
+	@PostMapping("/registerProduct")
+	public ResponseEntity<Object> registerProduct(@Valid @RequestBody ProductModel newProduct){
+		Optional<Object> objetcOptional = Optional.ofNullable(service.registerProduct(newProduct));
+				
+		if(objetcOptional.isEmpty()) {
+			return ResponseEntity.status(400).build();
+		} else {
+			return ResponseEntity.status(200).body(objetcOptional.get());
+		}
+		
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ project/src/main/java/com/plany/project/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.plany.project.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.plany.project.model.ProductModel;
+import com.plany.project.repository.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	private @Autowired ProductRepository repository;
+	
+	public Optional<?> registerProduct(ProductModel productToChange) {
+		return repository.findById(productToChange.getIdProduct()).map(productExists -> {
+			productExists.setName(productToChange.getName());
+			productExists.setDescription(productToChange.getDescription());
+			productExists.setPrice(productToChange.getPrice());
+			return Optional.ofNullable(repository.save(productExists));
+		}).orElseGet(() -> {
+			return Optional.empty();
+		});
+	}
+
+}
