f7035346d0fe97704e9d40d04ecfffbd1cdfc45d
Evelyn Carolina <evelyncarolina.contato@gmail.com>
2021-12-23 21:42:50 -0300

    [Feature - Task: Add Anotações @Repository]

--- project/src/main/java/com/plany/project/repository/ProductRepository.java
+++ project/src/main/java/com/plany/project/repository/ProductRepository.java
@@ -3,9 +3,11 @@ package com.plany.project.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.plany.project.model.ProductModel;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, Long> {
 	public List<ProductModel> findAllByNameContainingIgnoreCase(String name);
 }
--- project/src/main/java/com/plany/project/repository/UserRepository.java
+++ project/src/main/java/com/plany/project/repository/UserRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.plany.project.model.UserModel;
 
+@Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
 	public List<UserModel> findAllByNameContainingIgnoreCase(String name);
